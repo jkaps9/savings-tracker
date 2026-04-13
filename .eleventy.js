79b4a0d961046fc73dbd594dff1f36fe44df4f17
Josh <15219491+jkaps9@users.noreply.github.com>
2026-04-12 23:49:29 -0400

    update path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -49,7 +49,7 @@ export default function (config) {
 
   return {
     pathPrefix:
-      process.env.NODE_ENV === "production" ? "/eleventy-template/" : "/",
+      process.env.NODE_ENV === "production" ? "/savings-tracker/" : "/",
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
