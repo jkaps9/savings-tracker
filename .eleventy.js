81a281e956aa621fb67e4cb71f385ec50790e44b
Josh <15219491+jkaps9@users.noreply.github.com>
2026-04-13 23:48:19 -0400

    add title of detail page

--- .eleventy.js
+++ .eleventy.js
@@ -48,7 +48,7 @@ export default function (config) {
 
   // add date filter
   config.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
+    return DateTime.fromISO(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
   config.addFilter("dueDate", (dateStr) => {
